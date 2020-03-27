41b97a47735585ea346e3ddfea493913accb5d52
footprns <imankurniawan@gmail.com>
2020-03-27 15:52:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-# Configure the Azure Provider
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.0.0"
-  features {}
-}
+
 
 resource "azurerm_resource_group" "res_group" {
   location = "${var.location}"
